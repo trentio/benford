26800a453b7ed14249eaffcfd2085d666a0abede
tjohnson <trent.johnson@banno.com>
2020-08-20 22:00:42 -0500

    Sort the breakdown text

--- src/breakout.rs
+++ src/breakout.rs
@@ -1,20 +1,20 @@
 // Does a text breakdown of the data given via println
-
+use std::collections::HashMap;
 use itertools::Itertools;
 
 pub fn println_breakdown(digits: &Vec<usize>) {
-    let breakdown =
+    let breakdown: HashMap<&usize, Vec<usize>> =
         digits
             .into_iter()
             .map(|v| (v, 1))
-            .into_group_map();
+            .into_group_map()
+            ;
 
     let number_of_values = digits.len();
 
     println!("Breakdown:");
-    for (digit, list)  in breakdown.into_iter() {
+    for (digit, list) in breakdown.iter().sorted() {
         let total = list.len();
-
         let percent_of_total_values: f64 = total as f64 / number_of_values as f64;
 
         println!("Digit {}: {} total, {:.2} percent.", digit, total, percent_of_total_values)
